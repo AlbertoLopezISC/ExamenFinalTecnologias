65e145367957722370474fa33aa08408f1c4aeaf
Cynthia Delgado <c_indygirl@hotmail.com>
2020-06-27 02:18:48 -0500

    vista

--- angular/src/app/api1/api1.component.ts
+++ angular/src/app/api1/api1.component.ts
@@ -2,6 +2,7 @@ import { FormsModule } from '@angular/forms';
 import { Component, OnInit, NgModule } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { NodeapisService } from '../nodeapis.service';
+import swal from 'sweetalert2';
 
 @Component({
   selector: 'app-api1',
@@ -17,22 +18,28 @@ export class Api1Component implements OnInit {
 
   constructor(public apiserver: NodeapisService) { }
 
-  get(): void {
-    console.log(this.estado);
-    this.band = true;
-    /* let urlapi = ``;
-     this.apiserver.getapi1(urlapi).subscribe((data:any)=>{
-     });*/
-  }
 
-  onSubmit(userForm) {
-    console.log(this.estado);
-    this.apiserver.getapi1('http://localhost:3000/api/' + this.estado).subscribe((data: any) => {
-      this.dataEstado = data.datos[0];
-      this.band = true;
-      console.log(this.dataEstado);
-      console.log(this.dataEstado.viv);
-    });
+
+  onSubmit() {
+    if (this.estado) {
+      console.log(this.estado);
+      this.apiserver.getapi1('http://localhost:3000/api/' + this.estado).subscribe((data: any) => {
+        if (data.datos) {
+          this.dataEstado = data.datos[0];
+          console.log(this.dataEstado);
+          console.log(this.dataEstado.viv);
+        }
+        else {
+          swal.fire({ icon: 'error', title: 'Estado NO Encontrado' });
+          this.dataEstado=[];
+          console.log(this.dataEstado);
+        }
+
+      });
+    }
+    else {
+      swal.fire({ icon: 'error', title: 'Estado NO Encontrado' });
+    }
   }
 
 
--- angular/src/app/api2/api2.component.ts
+++ angular/src/app/api2/api2.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NodeapisService } from '../nodeapis.service';
+import swal from 'sweetalert2';
 @Component({
   selector: 'app-api2',
   templateUrl: './api2.component.html',
@@ -19,28 +20,21 @@ export class Api2Component implements OnInit {
   get(): void {
     if (this.id) {
       const n = parseInt(this.id, 10);
-      if (this.id.length === 2 && n > 0 && n < 32 && this.id) {
+      if (this.id.length === 2 && n > 0 && n <= 32 && this.id) {
         console.log(this.id);
         this.apiserver.getapi2('http://localhost:3000/api2/' + this.id).subscribe((data: any) => {
           this.band = true;
           this.municipios = data.datos;
           console.log(this.municipios);
-          // this.municipios[i].nom_agem;
-          // this.municipios[i].pob;
-          // this.municipios[i].pob_fem;
-          // this.municipios[i].pob_mas;
-          // this.municipios[i].viv;
         });
       } else {
-        alert('ID incorrecto');
+        swal.fire({ icon: 'error', title: 'Clave Incorrecta' });
+        this.municipios = [];
       }
     } else {
-      alert('ID incorrecto');
+      swal.fire({ icon: 'error', title: 'Clave Incorrecta' });
+      this.municipios = [];
     }
 
-    /* let urlapi = ``;
-     this.apiserver.getapi1(urlapi).subscribe((data:any)=>{
-
-     });*/
   }
 }
